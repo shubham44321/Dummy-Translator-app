3c9c4dd671a1069746196b30ca80e0b1bbdb06b4
shubham44321 <shubhampawar42@gmail.com>
2021-06-10 19:52:12 +0530

    ex 03

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
-const username =  prompt("Enter your name.");
-const welcomeMessage = `Welcome ${username}`;
-alert(welcomeMessage);
+const translateButton = document.querySelector("#btn-translate");
+
+translateButton.addEventListener("click",() => 
+    console.log("Clicked")
+);
