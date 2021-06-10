9c3c589ae88ccbbc7de6bb9cea9830fe9a6c8630
shubham44321 <shubhampawar42@gmail.com>
2021-06-10 21:00:05 +0530

    completed excesises

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
 const translateButton = document.querySelector("#btn-translate");
 const txtToTranslate = document.querySelector("#txt-to-translate");
 const translatedText = document.querySelector("#txt-translated"); 
+//const serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+const serverUrl = "https://api.funtranslations.com/translate/yoda.json";
 
-translateButton.addEventListener("click",() => {
+
+translateButton.addEventListener("click",async () => {
     let textToTranslate = txtToTranslate.value;
-    translatedText.innerText = textToTranslate;
+    const transalted = await getData(textToTranslate);
+    translatedText.innerText = transalted;
     //console.log(textToTranslate);
 });
+
+function appendtextToUrl(text){
+    return serverUrl+"?text="+text;
+}
+
+function errorHandler(ex){
+    console.log(`Error in fetching data : ${ex}`);
+}
+
+async function getData(text){
+    const url = encodeURI(appendtextToUrl(text));
+    //console.log(url);
+    const transaletedText = fetch(url)
+    .then(response => response.json())
+    .then(json => json.contents.translated)
+    .catch(errorHandler);
+    return await transaletedText;
+}
